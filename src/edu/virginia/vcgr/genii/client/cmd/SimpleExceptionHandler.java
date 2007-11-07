41b452b30982456ca5f5362bf767d3305d7fd75d
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2007-11-07 20:49:16 +0000

    Attempt to get more error information from BES.

--- src/edu/virginia/vcgr/genii/client/cmd/SimpleExceptionHandler.java
+++ src/edu/virginia/vcgr/genii/client/cmd/SimpleExceptionHandler.java
@@ -1,12 +1,18 @@
 package edu.virginia.vcgr.genii.client.cmd;
 
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
 public class SimpleExceptionHandler implements IExceptionHandler
 {
 	public int handleException(Throwable cause, PrintStream errorStream)
 	{
-		errorStream.println(cause.getLocalizedMessage());
+		if (cause instanceof NullPointerException)
+			errorStream.println("Internal Genesis II Error -- Null Pointer Exception");
+		else if (cause instanceof FileNotFoundException)
+			errorStream.println("File Not Found:  " + cause.getLocalizedMessage());
+		else
+			errorStream.println(cause.getLocalizedMessage());
 		
 		return 1;
 	}
