ff2964981f0e3b4ba8477f8c00230d60dab94036
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2007-11-08 19:46:20 +0000

    Partial transfer of OGRSH stuff.

--- OGRSH/java/jserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec java -Xms32M -Xmx128M -classpath "@JSERVER_DIRECTORY@/ext/bouncycastle/bcprov-jdk15-133.jar:@JSERVER_DIRECTORY@/ext/genii/GenesisII-security.jar:@JSERVER_DIRECTORY@/ext/genii/morgan-utilities.jar:@JSERVER_DIRECTORY@/lib:@JSERVER_DIRECTORY@/security" -Dlog4j.configuration=log4j.properties "-Dedu.virginia.vcgr.genii.install-base-dir=@JSERVER_DIRECTORY@" "-Dedu.virginia.vcgr.genii.client.configuration.base-dir=@JSERVER_DIRECTORY@" "-Dedu.virginia.vcgr.genii.client.configuration.user-dir=$GENII_USER_DIR" org.morgan.util.launcher.Launcher "@JSERVER_DIRECTORY@/jar-desc.xml" edu.virginia.vcgr.ogrsh.server.OGRSHServer "$@"
