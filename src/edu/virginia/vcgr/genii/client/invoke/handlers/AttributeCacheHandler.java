8655e1482dad7f3d721174ebf31f3b1da97ab6c1
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2007-11-13 18:14:29 +0000

    Working on attr caching.

--- src/edu/virginia/vcgr/genii/client/invoke/handlers/AttributeCacheHandler.java
+++ src/edu/virginia/vcgr/genii/client/invoke/handlers/AttributeCacheHandler.java
@@ -1,6 +1,5 @@
 package edu.virginia.vcgr.genii.client.invoke.handlers;
 
-import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -12,13 +11,11 @@ import org.apache.commons.logging.LogFactory;
 import org.ggf.rns.List;
 import org.ggf.rns.ListResponse;
 import org.ggf.rns.RNSPortType;
-import org.morgan.util.configuration.ConfigurationException;
 import org.oasis_open.docs.wsrf.rp_2.GetMultipleResourcePropertiesResponse;
 import org.oasis_open.docs.wsrf.rp_2.GetResourcePropertyResponse;
 import org.ws.addressing.EndpointReferenceType;
 
 import edu.virginia.vcgr.genii.client.cache.TimedOutLRUCache;
-import edu.virginia.vcgr.genii.client.comm.ClientUtils;
 import edu.virginia.vcgr.genii.client.invoke.InvocationContext;
 import edu.virginia.vcgr.genii.client.invoke.PipelineProcessor;
 import edu.virginia.vcgr.genii.client.naming.WSName;
@@ -27,7 +24,6 @@ import edu.virginia.vcgr.genii.common.rattrs.GetAttributesDocumentResponse;
 import edu.virginia.vcgr.genii.common.rattrs.GetAttributesResponse;
 import edu.virginia.vcgr.genii.common.rattrs.SetAttributes;
 import edu.virginia.vcgr.genii.common.rattrs.SetAttributesResponse;
-import edu.virginia.vcgr.genii.common.resource.ResourceUnknownFaultType;
 
 public class AttributeCacheHandler
 {
@@ -39,20 +35,23 @@ public class AttributeCacheHandler
 	private TimedOutLRUCache<WSName, CachedAttributeData> _attrCache =
 		new TimedOutLRUCache<WSName, CachedAttributeData>(_MAX_CACHE_ELEMENTS, _DEFAULT_TIMEOUT_MS);
 	
-	private CachedAttributeData fill(WSName name, InvocationContext ctxt) 
-		throws ResourceUnknownFaultType, RemoteException, ConfigurationException
+	private Collection<MessageElement> findAttributes(QName []attrs, CachedAttributeData data)
 	{
-		GeniiCommon proxy = ClientUtils.createProxy(
-			GeniiCommon.class, ctxt.getTarget(), ctxt.getCallingContext());
-		GetAttributesDocumentResponse resp = proxy.getAttributesDocument(null);
-		
-		CachedAttributeData data = new CachedAttributeData(resp);
-		synchronized(_attrCache)
+		Collection<MessageElement> ret = new ArrayList<MessageElement>();
+		for (QName attr : attrs)
 		{
-			_attrCache.put(name, data);
+			Collection<MessageElement> partial = data.getAttributes(attr);
+			if (partial == null)
+			{
+				if (!data.isFull())
+					return null;
+			} else
+			{
+				ret.addAll(partial);
+			}
 		}
 		
-		return data;
+		return ret;
 	}
 	
 	@PipelineProcessor(portType = GeniiCommon.class)
@@ -74,33 +73,23 @@ public class AttributeCacheHandler
 			data = _attrCache.get(name);
 		}
 		
-		Collection<MessageElement> ret = new ArrayList<MessageElement>();
-		boolean retry = true;
-		while (retry)
+		Collection<MessageElement> ret = null;
+		if (data != null)
+			ret = findAttributes(getAttributesRequest, data);
+		if (ret == null)
 		{
-			if (data == null)
-				data = fill(name, ctxt);
-			else
+			GetAttributesResponse resp = (GetAttributesResponse)ctxt.proceed();
+			data = new CachedAttributeData(resp);
+			synchronized(_attrCache)
 			{
-				retry = false;
-				ret.clear();
-				for (QName request : getAttributesRequest)
-				{
-					Collection<MessageElement> partial = data.getAttributes(request);
-					if (partial == null)
-					{
-						if (!data.isFull())
-						{
-							data = fill(name, ctxt);
-							retry = true;
-						}
-					} else
-					{
-						ret.addAll(partial);
-					}
-				}
+				_attrCache.put(name, data);
 			}
+			
+			ret = findAttributes(getAttributesRequest, data);
 		}
+		
+		if (ret == null)
+			return new GetAttributesResponse(new MessageElement[0]);;
 		
 		return new GetAttributesResponse(ret.toArray(new MessageElement[0]));
 	}
