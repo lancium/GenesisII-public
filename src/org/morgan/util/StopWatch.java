9afcfda06b997d43b58cde5b149ad527e1e04d5c
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2007-11-20 17:11:50 +0000

    timing.

--- src/org/morgan/util/StopWatch.java
+++ src/org/morgan/util/StopWatch.java
@@ -1,26 +1,24 @@
 package org.morgan.util;
 
-import java.util.Date;
-
 public class StopWatch
 {
 	private long _start;
 	
 	public StopWatch()
 	{
-		_start = System.currentTimeMillis();
+		_start = System.nanoTime();
 	}
 	
 	public void start()
 	{
-		_start = System.currentTimeMillis();
+		_start = System.nanoTime();
 	}
 	
 	public double lap()
 	{
-		long elapsed = System.currentTimeMillis() - _start;
-		double ret = elapsed / 1000.0;
-		_start = System.currentTimeMillis();
+		long elapsed = System.nanoTime() - _start;
+		double ret = elapsed / 1000000.0;
+		_start = System.nanoTime();
 		return ret;
 	}
 }
