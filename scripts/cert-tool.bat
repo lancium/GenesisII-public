d5012ea2f7d0474617992d1024dca57b58344ff4
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-01-31 15:58:10 +0000

    Changes to make the production system behave (and debug) easier.

--- scripts/cert-tool.bat
+++ scripts/cert-tool.bat
@@ -2,7 +2,7 @@
 set _CT_GENII_INSTALL_DIR=$INSTALL_PATH
 set _CT_LOCAL_JAVA_DIR=%_CT_GENII_INSTALL_DIR%\Java\windows-i586\jre
 
-"%_CT_LOCAL_JAVA_DIR%\bin\java.exe" -classpath "$INSTALL_PATH\ext\bouncycastle\bcprov-jdk15-133.jar;$INSTALL_PATH\lib\GenesisII-security.jar;$INSTALL_PATH\lib\morgan-utilities.jar;$INSTALL_PATH\lib;$INSTALL_PATH\security;$INSTALL_PATH\lib\GenesisII-client.jar" -Dlog4j.configuration=genesisII.log4j.properties "-Djava.library.path=$INSTALL_PATH\jni-lib" org.morgan.util.launcher.Launcher "$INSTALL_PATH\jar-desc.xml" edu.virginia.vcgr.genii.client.security.x509.CertTool %*
+"%_CT_LOCAL_JAVA_DIR%\bin\java.exe" -classpath "$INSTALL_PATH\ext\bouncycastle\bcprov-jdk15-133.jar;$INSTALL_PATH\lib\GenesisII-security.jar;$INSTALL_PATH\lib\morgan-utilities.jar;$INSTALL_PATH\lib;$INSTALL_PATH\security;$INSTALL_PATH\lib\GenesisII-client.jar" "-Dlog4j.configuration=$LOG4JCONFIG" "-Djava.library.path=$INSTALL_PATH\jni-lib" org.morgan.util.launcher.Launcher "$INSTALL_PATH\jar-desc.xml" edu.virginia.vcgr.genii.client.security.x509.CertTool %*
 
 set _CT_GENII_INSTALL_DIR=
 set _CT_LOCAL_JAVA_DIR=
--- scripts/grid.bat
+++ scripts/grid.bat
@@ -3,7 +3,7 @@
 set _GRID_GENII_INSTALL_DIR=$INSTALL_PATH
 set _GRID_LOCAL_JAVA_DIR=%_GRID_GENII_INSTALL_DIR%\Java\windows-i586\jre
 
-"%_GRID_LOCAL_JAVA_DIR%\bin\java.exe" -Xms32M -Xmx128M -classpath "$INSTALL_PATH\ext\bouncycastle\bcprov-jdk15-133.jar;$INSTALL_PATH\lib\GenesisII-security.jar;$INSTALL_PATH\lib\morgan-utilities.jar;$INSTALL_PATH\lib;$INSTALL_PATH\security;$INSTALL_PATH\lib\GenesisII-client.jar" -Dlog4j.configuration=genesisII.log4j.properties "-Djava.library.path=$INSTALL_PATH\jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=$INSTALL_PATH" org.morgan.util.launcher.Launcher "$INSTALL_PATH\jar-desc.xml" edu.virginia.vcgr.genii.client.cmd.Driver %*
+"%_GRID_LOCAL_JAVA_DIR%\bin\java.exe" -Xms32M -Xmx128M -classpath "$INSTALL_PATH\ext\bouncycastle\bcprov-jdk15-133.jar;$INSTALL_PATH\lib\GenesisII-security.jar;$INSTALL_PATH\lib\morgan-utilities.jar;$INSTALL_PATH\lib;$INSTALL_PATH\security;$INSTALL_PATH\lib\GenesisII-client.jar" "-Dlog4j.configuration=$LOG4JCONFIG" "-Djava.library.path=$INSTALL_PATH\jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=$INSTALL_PATH" org.morgan.util.launcher.Launcher "$INSTALL_PATH\jar-desc.xml" edu.virginia.vcgr.genii.client.cmd.Driver %*
 
 set _GRID_GENII_INSTALL_DIR=
 set _GRID_LOCAL_JAVA_DIR=
