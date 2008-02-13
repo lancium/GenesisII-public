f3db79376dd2a1fd5a200aead0680c92b8ce691a
cbs6n <cbs6n@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-02-13 20:39:26 +0000

    Changes in how user logs in with g-icing

--- src/edu/virginia/vcgr/genii/client/jni/gIIlib/JNILoginTool.java
+++ src/edu/virginia/vcgr/genii/client/jni/gIIlib/JNILoginTool.java
@@ -2,25 +2,12 @@ package edu.virginia.vcgr.genii.client.jni.gIIlib;
 
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-import edu.virginia.vcgr.genii.client.cmd.tools.GamlLoginTool;
-import edu.virginia.vcgr.genii.client.cmd.tools.gamllogin.AbstractGamlLoginHandler;
-import edu.virginia.vcgr.genii.client.cmd.tools.gamllogin.CertEntry;
-import edu.virginia.vcgr.genii.client.cmd.tools.gamllogin.GuiGamlLoginHandler;
-import edu.virginia.vcgr.genii.client.cmd.tools.gamllogin.TextGamlLoginHandler;
-import edu.virginia.vcgr.genii.client.context.CallingContextImpl;
+import edu.virginia.vcgr.genii.client.cmd.CommandLineRunner;
 import edu.virginia.vcgr.genii.client.context.ContextManager;
 import edu.virginia.vcgr.genii.client.context.ICallingContext;
 import edu.virginia.vcgr.genii.client.security.gamlauthz.TransientCredentials;
-import edu.virginia.vcgr.genii.client.security.gamlauthz.assertions.BasicConstraints;
-import edu.virginia.vcgr.genii.client.security.gamlauthz.assertions.RenewableAttributeAssertion;
-import edu.virginia.vcgr.genii.client.security.gamlauthz.assertions.RenewableClientAssertion;
-import edu.virginia.vcgr.genii.client.security.gamlauthz.assertions.RenewableClientAttribute;
-import edu.virginia.vcgr.genii.client.security.gamlauthz.assertions.RenewableIdentityAttribute;
-import edu.virginia.vcgr.genii.client.security.gamlauthz.identity.X509Identity;
-import edu.virginia.vcgr.genii.context.ContextType;
 
 
 public class JNILoginTool extends JNILibraryBase 
@@ -31,78 +18,95 @@ public class JNILoginTool extends JNILibraryBase
 	public static Boolean login(String keystorePath, String password, String certPattern){
 		tryToInitialize();
 		
+		CommandLineRunner runner = new CommandLineRunner();
+		String[] args = {"login"};						
 		
 		try{
-			if(keystorePath == null || keystorePath.length() == 0){	
-				System.out.println("Running GamlLoginTool");
-				GamlLoginTool loginTool = new GamlLoginTool();	
-				loginTool.setStoretype("WIN");
-				loginTool.run(System.out, System.err, 
-					new BufferedReader(new InputStreamReader(System.in)));
-				
-				/* Checks to make sure login worked */
-				ICallingContext callContext = ContextManager.getCurrentContext(false);
-				TransientCredentials transientCredentials = TransientCredentials
-				.getTransientCredentials(callContext);
-				
-				if(transientCredentials != null && transientCredentials._credentials != null &&
-						transientCredentials._credentials.size() > 0){
-					return true;
-				}
-				else{
-					return false;
-				}
+			runner.runCommand(args, System.out, System.err, new BufferedReader(new InputStreamReader(System.in)));
+			
+			// Checks to make sure login worked
+			ICallingContext callContext = ContextManager.getCurrentContext(false);
+			TransientCredentials transientCredentials = TransientCredentials
+			.getTransientCredentials(callContext);
+			
+			if(transientCredentials != null && transientCredentials._credentials != null &&
+					transientCredentials._credentials.size() > 0){
+				return true;
 			}
-			else{								
-				AbstractGamlLoginHandler handler = null;
-						
-				if (!useGui)
-					handler = new TextGamlLoginHandler(System.out, System.err, 
-							new BufferedReader(new InputStreamReader(System.in)));
-				else
-					handler = new GuiGamlLoginHandler(System.out, System.err, 
-							new BufferedReader(new InputStreamReader(System.in)));
-				
-				CertEntry certEntry = handler.selectCert(new FileInputStream(keystorePath), null, 
-						password, false, certPattern);
-				
-				if (certEntry == null)
-					return false;
-				
-				// Create identity assertion
-				RenewableIdentityAttribute identityAttr = new RenewableIdentityAttribute(
-					new BasicConstraints(
-							System.currentTimeMillis() - (1000L * 60 * 15), // 15 minutes ago
-							1000 * 60 * 60 * 2,								// valid 2 hours
-							10),
-					new X509Identity(certEntry._certChain));
-				RenewableAttributeAssertion identityAssertion =
-					new RenewableAttributeAssertion(identityAttr, certEntry._privateKey);
-				
-				// get the calling context (or create one if necessary)
-				ICallingContext callContext = ContextManager.getCurrentContext(false);
-				if (callContext == null)
-					callContext = new CallingContextImpl(new ContextType());
-				
-				// Delegate the identity assertion to the temporary client
-				// identity
-				RenewableClientAttribute delegatedAttr = new RenewableClientAttribute(
-					null, identityAssertion, callContext);
-				RenewableClientAssertion delegatedAssertion = new RenewableClientAssertion(
-					delegatedAttr, certEntry._privateKey);
-				
-				// insert the assertion into the calling context's transient creds
-				TransientCredentials transientCredentials =
-					TransientCredentials.getTransientCredentials(callContext);
-				transientCredentials._credentials.add(delegatedAssertion);
-				
-				ContextManager.storeCurrentContext(callContext);
-			}
-			return true;
+			else{
+				return false;
+			}		
 		}catch(Throwable e){
 			e.printStackTrace();
 			return false;			
 		}
-	}	
-	
+	}		
 }
+
+/*
+			if(keystorePath == null || keystorePath.length() == 0){	
+			System.out.println("Running GamlLoginTool");
+			GamlLoginTool loginTool = new GamlLoginTool();	
+			loginTool.setStoretype("WIN");
+			loginTool.run(System.out, System.err, 
+				new BufferedReader(new InputStreamReader(System.in)));
+			
+			// Checks to make sure login worked
+			ICallingContext callContext = ContextManager.getCurrentContext(false);
+			TransientCredentials transientCredentials = TransientCredentials
+			.getTransientCredentials(callContext);
+			
+			if(transientCredentials != null && transientCredentials._credentials != null &&
+					transientCredentials._credentials.size() > 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{								
+			AbstractGamlLoginHandler handler = null;
+					
+			if (!useGui)
+				handler = new TextGamlLoginHandler(System.out, System.err, 
+						new BufferedReader(new InputStreamReader(System.in)));
+			else
+				handler = new GuiGamlLoginHandler(System.out, System.err, 
+						new BufferedReader(new InputStreamReader(System.in)));
+			
+			CertEntry certEntry = handler.selectCert(new FileInputStream(keystorePath), null, 
+					password, false, certPattern);
+			
+			if (certEntry == null)
+				return false;
+			
+			// Create identity assertion
+			RenewableIdentityAttribute identityAttr = new RenewableIdentityAttribute(
+				new BasicConstraints(
+						System.currentTimeMillis() - (1000L * 60 * 15), // 15 minutes ago
+						1000 * 60 * 60 * 2,								// valid 2 hours
+						10),
+				new X509Identity(certEntry._certChain));
+			RenewableAttributeAssertion identityAssertion =
+				new RenewableAttributeAssertion(identityAttr, certEntry._privateKey);
+			
+			// get the calling context (or create one if necessary)
+			ICallingContext callContext = ContextManager.getCurrentContext(false);
+			if (callContext == null)
+				callContext = new CallingContextImpl(new ContextType());
+			
+			// Delegate the identity assertion to the temporary client
+			// identity
+			RenewableClientAttribute delegatedAttr = new RenewableClientAttribute(
+				null, identityAssertion, callContext);
+			RenewableClientAssertion delegatedAssertion = new RenewableClientAssertion(
+				delegatedAttr, certEntry._privateKey);
+			
+			// insert the assertion into the calling context's transient creds
+			TransientCredentials transientCredentials =
+				TransientCredentials.getTransientCredentials(callContext);
+			transientCredentials._credentials.add(delegatedAssertion);
+			
+			ContextManager.storeCurrentContext(callContext);
+		}
+*/
