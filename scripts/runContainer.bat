3a65dd70f8454c1cfb43c36da3126635c6390d1e
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-02-19 18:36:00 +0000

    Increased memory limits.

--- scripts/runContainer.bat
+++ scripts/runContainer.bat
@@ -3,7 +3,7 @@
 set _RC_GENII_INSTALL_DIR=$INSTALL_PATH
 set _RC_LOCAL_JAVA_DIR=%_RC_GENII_INSTALL_DIR%\Java\windows-i586\jre
 
-"%_RC_LOCAL_JAVA_DIR%\bin\java.exe" -Xms16M -Xmx128M -classpath "$INSTALL_PATH\ext\bouncycastle\bcprov-jdk15-133.jar;$INSTALL_PATH\lib\GenesisII-security.jar;$INSTALL_PATH\lib\morgan-utilities.jar;$INSTALL_PATH\lib;$INSTALL_PATH\security;$INSTALL_PATH\lib\GenesisII-client.jar" "-Dlog4j.configuration=$LOG4JCONFIG" "-Djava.library.path=$INSTALL_PATH\jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=$INSTALL_PATH" org.morgan.util.launcher.Launcher "$INSTALL_PATH\jar-desc.xml" edu.virginia.vcgr.genii.container.Container %*
+"%_RC_LOCAL_JAVA_DIR%\bin\java.exe" -Xms16M -Xmx512M -classpath "$INSTALL_PATH\ext\bouncycastle\bcprov-jdk15-133.jar;$INSTALL_PATH\lib\GenesisII-security.jar;$INSTALL_PATH\lib\morgan-utilities.jar;$INSTALL_PATH\lib;$INSTALL_PATH\security;$INSTALL_PATH\lib\GenesisII-client.jar" "-Dlog4j.configuration=$LOG4JCONFIG" "-Djava.library.path=$INSTALL_PATH\jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=$INSTALL_PATH" org.morgan.util.launcher.Launcher "$INSTALL_PATH\jar-desc.xml" edu.virginia.vcgr.genii.container.Container %*
 
 set _RC_GENII_INSTALL_DIR=
 set _RC_LOCAL_JAVA_DIR=
--- scripts/runContainer.sh
+++ scripts/runContainer.sh
@@ -3,4 +3,4 @@
 _RC_GENII_INSTALL_DIR="%{INSTALL_PATH}"
 _RC_LOCAL_JAVA_DIR="$_RC_GENII_INSTALL_DIR/Java/linux-i586/jre"
 
-exec "$_RC_LOCAL_JAVA_DIR/bin/java" -Xms16M -Xmx128M -classpath "%{INSTALL_PATH}/ext/bouncycastle/bcprov-jdk15-133.jar:%{INSTALL_PATH}/lib/GenesisII-security.jar:%{INSTALL_PATH}/lib/morgan-utilities.jar:%{INSTALL_PATH}/lib:%{INSTALL_PATH}/security" "-Dlog4j.configuration=%{LOG4JCONFIG}" "-Djava.library.path=%{INSTALL_PATH}/jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=%{INSTALL_PATH}" org.morgan.util.launcher.Launcher "%{INSTALL_PATH}/jar-desc.xml" edu.virginia.vcgr.genii.container.Container "$@"
+exec "$_RC_LOCAL_JAVA_DIR/bin/java" -Xms16M -Xmx512M -classpath "%{INSTALL_PATH}/ext/bouncycastle/bcprov-jdk15-133.jar:%{INSTALL_PATH}/lib/GenesisII-security.jar:%{INSTALL_PATH}/lib/morgan-utilities.jar:%{INSTALL_PATH}/lib:%{INSTALL_PATH}/security" "-Dlog4j.configuration=%{LOG4JCONFIG}" "-Djava.library.path=%{INSTALL_PATH}/jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=%{INSTALL_PATH}" org.morgan.util.launcher.Launcher "%{INSTALL_PATH}/jar-desc.xml" edu.virginia.vcgr.genii.container.Container "$@"
