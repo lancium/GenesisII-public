3f522e10497d2c99e1dbe1ea4965c0cf4205a7a1
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-02-26 22:28:32 +0000

    Starting to get the OGSA WSRF Interop Tool in order.

--- src/edu/virginia/vcgr/genii/client/cmd/tools/OGSAWSRFBPInteropTool.java
+++ src/edu/virginia/vcgr/genii/client/cmd/tools/OGSAWSRFBPInteropTool.java
@@ -1,60 +1,205 @@
 package edu.virginia.vcgr.genii.client.cmd.tools;
 
-import javax.xml.namespace.QName;
+import java.rmi.RemoteException;
 
+import org.morgan.util.configuration.ConfigurationException;
 import org.ogf.ogsa.ticker.CreateTicker;
 import org.ogf.ogsa.ticker.TickerFactory;
 import org.ws.addressing.EndpointReferenceType;
 
+import edu.virginia.vcgr.genii.client.cmd.InvalidToolUsageException;
 import edu.virginia.vcgr.genii.client.cmd.ToolException;
 import edu.virginia.vcgr.genii.client.comm.ClientUtils;
-import edu.virginia.vcgr.genii.client.naming.EPRUtils;
-import edu.virginia.vcgr.genii.client.ogsa.OGSARP;
-import edu.virginia.vcgr.genii.client.rp.ResourcePropertyManager;
+import edu.virginia.vcgr.genii.client.rns.RNSException;
+import edu.virginia.vcgr.genii.client.rns.RNSPath;
+import edu.virginia.vcgr.genii.client.rns.RNSPathQueryFlags;
 
 public class OGSAWSRFBPInteropTool extends BaseGridTool
 {
+	static private final String _DESCRIPTION =
+		"Participates in the OGSA-WSRF-BP Interop Festival.";
+	static private final String _USAGE_RESOURCE =
+		"ogsa-bp-interop <ticker-factory-path> test1 ... test8";
+	
 	public OGSAWSRFBPInteropTool()
 	{
-		super("OGSA WSRF-BP Interop Tool", "ogsa-bp-interop", true);
+		super(_DESCRIPTION, _USAGE_RESOURCE, true);
+	}
+	
+	static private EndpointReferenceType lookupTickerFactory(String path)
+		throws RNSException, ConfigurationException
+	{
+		return RNSPath.getCurrent().lookup(
+			path, RNSPathQueryFlags.MUST_EXIST).getEndpoint();
+	}
+	
+	static private TickerFactory createTicker(TickerFactory factory)
+		throws RemoteException, ConfigurationException
+	{
+		EndpointReferenceType epr = factory.createTicker(new CreateTicker()).getTickerReference();
+		return ClientUtils.createProxy(TickerFactory.class, epr);
+	}
+	
+	static private void terminateTicker(TickerFactory ticker)
+	{
+		// TODO
+	}
+	
+	private void runTest1(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest2(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest3(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest4(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest5(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest6(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest7(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
+	}
+	
+	private void runTest8(TickerFactory factory) throws Throwable
+	{
+		stdout.print("\tCreating ticker...");
+		TickerFactory ticker = createTicker(factory);
+		stdout.println("Done");
+		
+		// TODO
+		
+		stdout.print("\tTerminating ticker...");
+		terminateTicker(ticker);
+		stdout.println("Done");
 	}
 	
 	@Override
 	protected int runCommand() throws Throwable
 	{
-		/*
-		TickerFactory factory = ClientUtils.createProxy(TickerFactory.class,
-			EPRUtils.makeEPR("http://193.122.18.162:8080/ogsabp-interop/ticker-factory", false));
-		*/
-		/*
-		TickerFactory factory = ClientUtils.createProxy(TickerFactory.class,
-			EPRUtils.makeEPR("http://zam461.zam.kfa-juelich.de:9126/services/TickerFactoryService", false));
-		*/
-		TickerFactory factory = ClientUtils.createProxy(TickerFactory.class,
-			EPRUtils.makeEPR("https://localhost:18080/axis/services/TickerFactory", false));
-			
-		EndpointReferenceType epr = factory.createTicker(new CreateTicker()).getTickerReference();
-		OGSARP rp = (OGSARP)ResourcePropertyManager.createRPInterface(epr, OGSARP.class); 
-		for (QName rpname : rp.getResourcePropertyNames())
+		EndpointReferenceType tickerFactoryEPR = 
+			lookupTickerFactory(getArgument(0));
+		TickerFactory tickerFactory = ClientUtils.createProxy(
+			TickerFactory.class, tickerFactoryEPR);
+		
+		for (int lcv = 1; lcv < numArguments(); lcv++)
 		{
-			System.err.println("ResourceProperty:  " + rpname);
+			String test = getArgument(lcv);
+			try
+			{
+				stdout.println("Running test \"" + test + "\".");
+				if (test.equals("test1"))
+					runTest1(tickerFactory);
+				else if (test.equals("test2"))
+					runTest2(tickerFactory);
+				else if (test.equals("test3"))
+					runTest3(tickerFactory);
+				else if (test.equals("test4"))
+					runTest4(tickerFactory);
+				else if (test.equals("test5"))
+					runTest5(tickerFactory);
+				else if (test.equals("test6"))
+					runTest6(tickerFactory);
+				else if (test.equals("test7"))
+					runTest7(tickerFactory);
+				else if (test.equals("test8"))
+					runTest8(tickerFactory);
+				else
+				{
+					stderr.println("Test \"" + test + "\" is not known.");
+				}
+			}
+			catch (Throwable cause)
+			{
+				stderr.println("Test \"" + test + "\" threw exception.");
+				cause.printStackTrace(stderr);
+			}
+			
+			stdout.println();
 		}
 		
-		/*
-		TickerFactory ticker = ClientUtils.createProxy(TickerFactory.class, epr);
-		
-		OutputStreamWriter writer = new OutputStreamWriter(System.out);
-		ObjectSerializer.serialize(writer,
-			ticker.getResourceProperty(OGSAWSRFBPConstants.RESOURCE_PROPERTY_NAMES_ATTR_QNAME),
-			OGSAWSRFBPConstants.RESOURCE_PROPERTY_NAMES_ATTR_QNAME);
-		
-		writer.flush();
-		*/
 		return 0;
 	}
 
 	@Override
 	protected void verify() throws ToolException
 	{
+		if (numArguments() < 1)
+			throw new InvalidToolUsageException("Too few arguments.");
 	}
 }
