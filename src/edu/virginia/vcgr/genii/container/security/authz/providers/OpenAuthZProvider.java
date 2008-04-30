519062656f9e42211cfb64b911a8e99104a49db2
dgm4d <dgm4d@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-04-30 18:00:43 +0000

    Updates to allow no-sec

---
+++ src/edu/virginia/vcgr/genii/container/security/authz/providers/OpenAuthZProvider.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2006 University of Virginia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package edu.virginia.vcgr.genii.container.security.authz.providers;
+
+import java.security.cert.PKIXBuilderParameters;
+import java.security.cert.X509CertSelector;
+import java.security.cert.X509Certificate;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
+
+import java.util.*;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+
+import javax.net.ssl.CertPathTrustManagerParameters;
+import javax.net.ssl.ManagerFactoryParameters;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.morgan.util.configuration.ConfigurationException;
+
+import edu.virginia.vcgr.genii.client.context.*;
+import edu.virginia.vcgr.genii.client.security.MessageLevelSecurity;
+import edu.virginia.vcgr.genii.common.security.*;
+import edu.virginia.vcgr.genii.client.security.authz.RWXCategory;
+import edu.virginia.vcgr.genii.client.security.authz.RWXManager;
+import edu.virginia.vcgr.genii.client.security.gamlauthz.*;
+import edu.virginia.vcgr.genii.client.security.gamlauthz.assertions.*;
+import edu.virginia.vcgr.genii.client.security.gamlauthz.identity.*;
+import edu.virginia.vcgr.genii.container.resource.*;
+import edu.virginia.vcgr.genii.client.resource.*;
+import edu.virginia.vcgr.genii.container.Container;
+import edu.virginia.vcgr.genii.client.security.x509.KeyAndCertMaterial;
+
+
+
+/**
+ * 
+ * NOTES: - Returns true for all access-control decisions
+ * 
+ * @author dmerrill
+ * 
+ */
+public class OpenAuthZProvider implements IAuthZProvider {
+
+	static public final String GAML_ACL_PROPERTY_NAME = "genii.container.security.authz.gaml-acl";
+
+	static protected final MessageLevelSecurity _defaultMinMsgSec = new MessageLevelSecurity(
+			MessageLevelSecurity.NONE);
+
+	@SuppressWarnings("unused")
+	static private Log _logger = LogFactory.getLog(OpenAuthZProvider.class);
+	
+	public OpenAuthZProvider() {}
+	
+	/**
+	 * Presently configures the specified resource to have default access allowed
+	 * for every GAML credential in the bag of credentials.  We may want to look
+	 * at restricting this in the future to special credentials.
+	 */
+	public void setDefaultAccess(
+			ICallingContext callingContext,
+			IResource resource,
+			X509Certificate[] serviceCertChain) throws AuthZSecurityException,
+			ResourceException {
+
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	public boolean checkAccess(
+			ICallingContext callingContext,
+			X509Certificate callerCert, 
+			IResource resource, 
+			Method operation)
+				throws AuthZSecurityException, ResourceException {
+
+		return true;
+	}
+
+	public MessageLevelSecurity getMinIncomingMsgLevelSecurity(
+			IResource resource) throws AuthZSecurityException,
+			ResourceException {
+
+		return _defaultMinMsgSec;
+	}
+
+	public AuthZConfig getAuthZConfig(IResource resource)
+			throws AuthZSecurityException, ResourceException {
+
+		return new AuthZConfig(null);
+	}
+
+	public void setAuthZConfig(AuthZConfig config, IResource resource)
+			throws AuthZSecurityException, ResourceException {
+
+	}
+
+}
