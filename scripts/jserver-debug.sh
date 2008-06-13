4a7bb253c71777921ec37a0a7e1f478bae28f200
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-06-13 16:54:49 +0000

    A few minor changes to bring the new app watcher stuff up to speed in the system.

--- scripts/jserver-debug.sh
+++ scripts/jserver-debug.sh
@@ -3,4 +3,4 @@
 _RC_GENII_INSTALL_DIR="%{INSTALL_PATH}"
 _RC_LOCAL_JAVA_DIR="$_RC_GENII_INSTALL_DIR/Java/linux-i586/jre"
 
-exec "$_RC_LOCAL_JAVA_DIR/bin/java" -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n -Xms16M -Xmx128M -classpath "%{INSTALL_PATH}/ext/bouncycastle/bcprov-jdk15-133.jar:%{INSTALL_PATH}/lib/GenesisII-security.jar:%{INSTALL_PATH}/lib/morgan-utilities.jar:%{INSTALL_PATH}/lib:%{INSTALL_PATH}/security" "-Dlog4j.configuration=%{LOG4JCONFIG}" "-Djava.library.path=%{INSTALL_PATH}/jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=%{INSTALL_PATH}" org.morgan.util.launcher.Launcher "%{INSTALL_PATH}/jserver-jar-desc.xml" edu.virginia.vcgr.ogrsh.server.OGRSHServer "$@"
+exec "$_RC_LOCAL_JAVA_DIR/bin/java" -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n -Xms16M -Xmx512M -classpath "%{INSTALL_PATH}/lib:%{INSTALL_PATH}/ApplicationWatcher/app-watcher.jar" "-Dlog4j.configuration=%{LOG4JCONFIG}" "-Djava.library.path=%{INSTALL_PATH}/jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=%{INSTALL_PATH}" edu.virginia.vcgr.appwatcher.ApplicationLauncher "%{INSTALL_PATH}/AppilcationWatcher/genii-ogrshserv-application.properties "$@"
