ee65c2b7e5e5f05d927fee724af160db718ca8a3
mmm2a <mmm2a@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2008-07-14 18:19:26 +0000

    More utilities for fuse.

---
+++ src/edu/virginia/vcgr/fuse/FuseUtils.java
@@ -0,0 +1,43 @@
+package edu.virginia.vcgr.fuse;
+
+import java.io.File;
+
+import edu.virginia.vcgr.genii.client.utils.SystemExec;
+
+public class FuseUtils
+{
+	static public String checkForFuserMount()
+	{
+		File f = SystemExec.findExecutableInPath("fusermount");
+		if (f == null)
+			return "Unable to locate fusermount binary.";
+		
+		return null;
+	}
+	
+	static private String checkForFuseDevice()
+	{
+		File f = new File("/dev/fuse");
+		if (!f.exists())
+			return "Couldn't locate /dev/fuse.";
+		if (!f.canRead())
+			return "Can't read /dev/fuse.";
+		if (!f.canWrite())
+			return "Can't write /dev/fuse.";
+		
+		return null;
+	}
+	
+	static public String supportsFuse()
+	{
+		String msg = checkForFuserMount();
+		if (msg != null)
+			return msg;
+		
+		msg = checkForFuseDevice();
+		if (msg != null)
+			return msg;
+		
+		return null;
+	}
+}
--- src/edu/virginia/vcgr/genii/client/utils/SystemExec.java
+++ src/edu/virginia/vcgr/genii/client/utils/SystemExec.java
@@ -1,16 +1,35 @@
 package edu.virginia.vcgr.genii.client.utils;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.regex.Pattern;
 
 import org.morgan.util.io.StreamUtils;
 
 public class SystemExec
 {
+	static public File findExecutableInPath(String executableName)
+	{
+		String path = System.getenv(executableName);
+		for (String pathElement : path.split(
+			Pattern.quote(File.pathSeparator)))
+		{
+			if (pathElement != null && pathElement.length() > 0)
+			{
+				File f = new File(pathElement, executableName);
+				if (f.exists() && f.canExecute())
+					return f;
+			}
+		}
+		
+		return null;
+	}
+	
 	static public String executeForOutput(String...command)
 		throws IOException
 	{
