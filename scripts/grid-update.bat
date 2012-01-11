59f03f9fdbaf6478227ee1cfb968c7e5d601cd4c
mts5x <mts5x@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2012-01-11 18:23:56 +0000

    updated ant scripts to allow
    clean-scripts
    build-scripts
    
    without having to rebuild entire project
    
    Reorganized jni, allowing for both 64(ant option)/32 (default) bit linux project to be built
    
    updated windows bat files to use updated paths

--- scripts/grid-update.bat
+++ scripts/grid-update.bat
@@ -1,9 +1,8 @@
 @ECHO OFF
 
 set _GRID_GENII_INSTALL_DIR=$INSTALL_PATH
-set _GRID_LOCAL_JAVA_DIR=%_GRID_GENII_INSTALL_DIR%\Java\windows-i586\jre
 
-"%_GRID_LOCAL_JAVA_DIR%\bin\java.exe" -Xms32M -Xmx512M -classpath "$INSTALL_PATH\lib;$INSTALL_PATH\ApplicationWatcher\app-manager.jar" -Dedu.virginia.vcgr.appwatcher.update-frequency=604800000 "-Dlog4j.configuration=$LOG4JCONFIG" "-Djava.library.path=$INSTALL_PATH\jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=$INSTALL_PATH" edu.virginia.vcgr.appmgr.launcher.ApplicationLauncher "$INSTALL_PATH\ApplicationWatcher\genii-update-application.properties" %*
+"%JAVA_HOME%\bin\java.exe" -Xms32M -Xmx512M -classpath "$INSTALL_PATH\lib;$INSTALL_PATH\ApplicationWatcher\app-manager.jar" -Dedu.virginia.vcgr.appwatcher.update-frequency=604800000 "-Dlog4j.configuration=$LOG4JCONFIG" "-Djava.library.path=$INSTALL_PATH\%{GENII_JNI_PATH}" "-Dedu.virginia.vcgr.genii.install-base-dir=$INSTALL_PATH" edu.virginia.vcgr.appmgr.launcher.ApplicationLauncher "$INSTALL_PATH\ApplicationWatcher\genii-update-application.properties" %*
 
 set _GRID_GENII_INSTALL_DIR=
 set _GRID_LOCAL_JAVA_DIR=
