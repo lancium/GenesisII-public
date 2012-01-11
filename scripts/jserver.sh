4040f4832e4f3ab2511edf00130ce35aa4c96690
mts5x <mts5x@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2012-01-11 16:23:32 +0000

    updated linux scripts to respect JAVA_HOME instead of using jvm in svn
    also removed some old/broken scripts from being generated through ant and deleted several.
    
    JNI directory now configurable through ant for linux

--- scripts/jserver.sh
+++ scripts/jserver.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 _RC_GENII_INSTALL_DIR="%{INSTALL_PATH}"
-_RC_LOCAL_JAVA_DIR="$_RC_GENII_INSTALL_DIR/Java/linux-i586/jre"
 
-exec "$_RC_LOCAL_JAVA_DIR/bin/java" -Xms16M -Xmx512M -classpath "%{INSTALL_PATH}/lib:%{INSTALL_PATH}/ApplicationWatcher/app-manager.jar" "-Dlog4j.configuration=%{LOG4JCONFIG}" "-Djava.library.path=%{INSTALL_PATH}/jni-lib" "-Dedu.virginia.vcgr.genii.install-base-dir=%{INSTALL_PATH}" edu.virginia.vcgr.appmgr.launcher.ApplicationLauncher "%{INSTALL_PATH}/ApplicationWatcher/genii-ogrshserv-application.properties" "$@"
+exec "$JAVA_HOME/bin/java" -Xms16M -Xmx512M -classpath "%{INSTALL_PATH}/lib:%{INSTALL_PATH}/ApplicationWatcher/app-manager.jar" "-Dlog4j.configuration=%{LOG4JCONFIG}" "-Djava.library.path=%{INSTALL_PATH}/%{GENII_JNI_PATH}" "-Dedu.virginia.vcgr.genii.install-base-dir=%{INSTALL_PATH}" edu.virginia.vcgr.appmgr.launcher.ApplicationLauncher "%{INSTALL_PATH}/ApplicationWatcher/genii-ogrshserv-application.properties" "$@"
