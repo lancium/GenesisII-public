c17bd672125f538e9a3202485af706322b682351
cak0l <cak0l@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2012-03-23 17:05:39 +0000

    updated with a tag for the package being produced. originally came from ticket #57. checking in directly rather than delaying work that's in progress because (1) this ticket was already reviewed, (2) the code is a minor change, (3) the script is not used anywhere externally and is solely for our own build processes.

--- scripts/produce_build_package.sh
+++ scripts/produce_build_package.sh
@@ -8,11 +8,16 @@ export WORKDIR="$( \cd "$(\dirname "$0")" && \pwd )"  # obtain the script's work
 
 build_folder="$1"; shift
 storage_folder="$1"; shift
+additional_tag="$1"; shift  # optional
 if [ ! -d "$build_folder" -o ! -d "$storage_folder" ]; then
   echo This script packs up a build folder after an GenesisII build.
   echo It needs two folders: the first pointing at where the build resides, and
   echo the second specifying where to store the archive.
   exit 1
+fi
+if [ ! -z "$additional_tag" ]; then
+  # add a separator character.
+  additional_tag="-$additional_tag"
 fi
 
 pushd $build_folder &>/dev/null
@@ -22,7 +27,7 @@ date_string="$(date +"%Y_%b_%e_%H%M" | sed -e 's/ //g')"
 
 EXCLUDES=(--exclude=".svn" --exclude="*.class" --exclude="*.log" --exclude="*.log.*" )
 
-tar -czf "$storage_folder/GenesisII_build_${date_string}.tar.gz" . ${EXCLUDES[*]}
+tar -czf "$storage_folder/GenesisII-build-${date_string}${additional_tag}.tar.gz" . ${EXCLUDES[*]}
 
 popd &>/dev/null
 
