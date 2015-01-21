e2b6a2c7ea7365deb8d6fa6d3adce1b95a3e6972
cak0l <cak0l@31b5fc62-7e36-0410-b1c6-c26cd53d3853>
2015-01-21 21:57:33 +0000

    simplified this down to what's needed at xsede hopefully.

--- installer/rpm-building/copy_development_to_production.sh
+++ installer/rpm-building/copy_development_to_production.sh
@@ -6,19 +6,17 @@
 
 HERE="$( \cd "$(\dirname "$0")" && \pwd )"
 
-RPM_LOCATION="$HOME/installer_products"
+PKG=genesis2-xsede
+#GRID=xsede
 
-DEFAULT_FILE_PERMS="664"
-DEFAULT_DIR_PERMS="775"
 REPO_HOSTNAME=software.xsede.org
-DEV_PKG_BASE=/local/software.xsede.org/development/$PKG
-DEV_RPM_BASE=/local/software.xsede.org/development/repo
+#DEV_PKG_BASE=/local/software.xsede.org/development/$PKG
+#DEV_RPM_BASE=/local/software.xsede.org/development/repo
 PROD_RPM_BASE=/local/software.xsede.org/production/repo
-PKG_SUBDIR=$PKG-$VER-$REL
 TEST_OPTION=""
-CMD=$(basename $0)
+#CMD=$(basename $0)
 
-USAGE="This program will copy RPMs from the XSEDE YUM repository for development into\nthe production repository.\nIt supports the following flags:\n\t-h help\n\t-u username on repository\n\t-l path where local RPMs are located"
+USAGE="This program will copy RPMs from the XSEDE YUM repository for development into\nthe production repository.\nIt supports the following flags:\n\t-h help\n\t-u username on repository\nIt also takes two arguments, the version number and release number for\nthe Genesis II GFFS release that should be copied."
 
 # process command line arguments...
 
@@ -28,8 +26,6 @@ while getopts "thu:l:" opt; do
        PROD_RPM_BASE="/local/software.xsede.org/testing/repo"
       ;;
     u) REPO_HOSTNAME="$OPTARG@$REPO_HOSTNAME"
-       ;;
-    l) RPM_LOCATION="$OPTARG"
        ;;
     h) 
       echo -e $USAGE 
@@ -42,36 +38,20 @@ while getopts "thu:l:" opt; do
   esac
 done
 
-# verify the variables we were given and build derived variables...
+# scoot past the options we consumed.
+shift $((OPTIND-1))
 
-if [ ! -d $RPM_LOCATION ]; then
-  echo -e $USAGE
+VER="$1"; shift
+REL="$1"; shift
+
+#echo "got ver '$VER' and rel '$REL'"
+
+if [ -z "$VER" -o -z "$REL" ]; then
+  echo -e $USAGE >&2
   echo
-  echo "The local path for the RPMS of: $RPM_LOCATION"
-  echo "does not exist.  Please specify a different location where the RPMs"
-  echo "can be found."
+  echo The version or release number was not specified on the command line.
   exit 1
 fi
-
-VERFILE="$RPM_LOCATION/current.version"
-DEPFILE="$RPM_LOCATION/current.deployment"
-
-if [ ! -e $VERFILE ]; then
-  echo -e $USAGE
-  echo
-  echo "The installers do not seem to have been built; the file:"
-  echo "   $VERFILE"
-  echo "must exist before running this script, and it is created automatically"
-  echo "by the GFFS build_installer script."
-  exit 1
-fi
-
-PKG=genesis2
-VER=$(sed -n -e 's/genii.app-version=\(.*\)/\1/p' "$VERFILE")
-REL=$(sed -n -e 's/genii.release-number=\(.*\)/\1/p' "$VERFILE")
-GRID=$(sed -n -e 's/genii.simple-name=\(.*\)/\1/p' "$DEPFILE")
-
-PKG_SUBDIR=$PKG-$VER-$REL
 
 ###########################################
 # Sign Development RPMs
