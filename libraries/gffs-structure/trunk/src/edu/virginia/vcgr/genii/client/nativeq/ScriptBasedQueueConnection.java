a6cb33384918e39d9eadbc0ca1f60947d04d4780
landon912 <landonaddison@yahoo.com>
2020-03-25 21:00:40 -0400

    fixed typo in sbatch trapping SIGTERM

--- libraries/gffs-structure/trunk/src/edu/virginia/vcgr/genii/client/nativeq/ScriptBasedQueueConnection.java
+++ libraries/gffs-structure/trunk/src/edu/virginia/vcgr/genii/client/nativeq/ScriptBasedQueueConnection.java
@@ -164,6 +164,10 @@ public abstract class ScriptBasedQueueConnection<ProviderConfigType extends Scri
 				script.format("trap signalTrap %s\n", signal);
 		}
 
+		// LAK: 13 March: Added so that the batch script does not exit when it gets a SIG_TERM, instead it lets its children
+		// repond to the signal and then gracefully exit after the children processes 
+		script.format("trap : SIGTERM\n");
+
 		script.format("export QUEUE_SCRIPT_RESULT=0\n");
 	}
 
@@ -186,10 +190,6 @@ public abstract class ScriptBasedQueueConnection<ProviderConfigType extends Scri
 	{
 		Set<UnixSignals> signals = queueConfiguration().trapSignals();
 		List<String> newCmdLine = new Vector<String>();
-		
-		// LAK: 13 March: Added so that the batch script does not exit when it gets a SIG_TERM, instead it lets its children
-		// repond to the signal and then gracefully exit after the children processes 
-		// script.format("trap : SIGTERM\n");
 
 		script.format("cd \"%s\"\n", workingDirectory.getAbsolutePath());
 
