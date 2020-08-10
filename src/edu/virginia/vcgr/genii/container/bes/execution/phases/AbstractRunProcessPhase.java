087f5700208c441626b39f926d586ddc304865d4
cchoughton <charliehoughton7@gmail.com>
2020-08-10 15:46:22 -0400

    Fix json formatting for properties.json

--- src/edu/virginia/vcgr/genii/container/bes/execution/phases/AbstractRunProcessPhase.java
+++ src/edu/virginia/vcgr/genii/container/bes/execution/phases/AbstractRunProcessPhase.java
@@ -85,9 +85,9 @@ abstract class AbstractRunProcessPhase extends AbstractExecutionPhase
 
 					output.write("\"numProcesses\": \"" + numProcesses + "\",\n");
 					output.write("\"NumProcessorsPerHost\": \"" + numProcessesPerHost + "\",\n");
-					output.write("\"threadsPerProcess\": \"" + threadsPerProcess + "\"\n");
-					output.write("\"jobAnnotation\": \"" + activity.getJobAnnotation() + "\"\n");
-					output.write("\"gpuType\": \"" + activity.getGPUType() + "\"\n");
+					output.write("\"threadsPerProcess\": \"" + threadsPerProcess + "\",\n");
+					output.write("\"jobAnnotation\": \"" + activity.getJobAnnotation() + "\",\n");
+					output.write("\"gpuType\": \"" + activity.getGPUType() + "\",\n");
 					output.write("\"gpuCount\": \"" + activity.getGPUCount() + "\"\n");
 					output.write("}\n");
 				} catch ( IOException e ) {
